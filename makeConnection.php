2a90763db75b43d3cd0ac30e72a1909701f746fb
faizanahmedkabuji <59233962+FaizanAhmedKabuji@users.noreply.github.com>
2022-05-01 22:19:02 +0530

    Update makeConnection.php

--- makeConnection.php
+++ makeConnection.php
@@ -1,4 +1,4 @@
 <?php
-	$connect=mysqli_connect('localhost','root','hellozima') or die('Cannot connect to server');
+	$connect=mysqli_connect('localhost','root','enter_your_password_here') or die('Cannot connect to server');
 	mysqli_select_db($connect,'ccmsdatabase') or die ('Cannot find database');
 ?>
